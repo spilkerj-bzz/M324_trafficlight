9bf7bc3fef3740f0e2be0d9cc21f08fffd129ca4
non euclidean <spilkerj@bzz.ch>
2025-01-08 10:58:28 +0100

    full refractor

--- trafficlight.test.js
+++
@@ -1,7 +0,0 @@
-const app = require("./trafficlight");
-
-describe('test suite traffic light', () => {
-    it(`case red light`, () => {
-        expect(app.trafficLight("red")).toBe("DON'T WALK!");
-    });
-});
